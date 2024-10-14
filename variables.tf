379df1e7fa9d63369730c405fd3604614c16291f
ashok <ashoknarne@gmail.com>
2024-10-14 12:27:47 +1100

    security group creation module

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    
+}
+
+variable "environment" {
+    
+}
+
+variable "common_tags" {
+    default = { }
+}
+
+variable "sg_tags" {
+    default = {}
+}
+
+variable "vpc_id" {
+
+}
+
+variable "sg_name" {
+    
+}
